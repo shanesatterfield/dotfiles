28bb77c4094ddcdfd5e4e53d626edc90cfb54367
Shane Satterfield <dustyplant@gmail.com>
2017-04-20 21:34:23 -0700

    Added zsh config and bootstrap script that currently just creates symlinks.

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create symlinks to dotfiles
+ln -s $SRC_DIR/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $SRC_DIR/nvim/init.vim ~/.vimrc
+ln -s $SRC_DIR/nvim/init.vim ~/.ideavimrc
+
+ln -s $SRC_DIR/zsh/zshrc ~/.zshrc
